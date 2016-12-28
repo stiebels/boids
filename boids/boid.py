15176a456c65eb91cee9d33ac2bbb9deca657309
stiebels <simon.stiebellehner@gmail.com>
2016-12-28 17:47:09 +0100

    converted magic numbers to variables

--- boids/boid.py
+++ boids/boid.py
@@ -3,35 +3,47 @@ from matplotlib import animation
 import random
 
 
-boids_x=[random.uniform(-450,50.0) for x in range(50)]
-boids_y=[random.uniform(300.0,600.0) for x in range(50)]
-boid_x_velocities=[random.uniform(0,10.0) for x in range(50)]
-boid_y_velocities=[random.uniform(-20.0,20.0) for x in range(50)]
-boids=(boids_x,boids_y,boid_x_velocities,boid_y_velocities)
+flock_size = 50
+init_x_coord = (-450, 50)
+init_y_coord = (300, 600)
+init_x_velo = (0, 10)
+init_y_velo = (-20, 20)
+avoid_distance = 100
+flying_distance = 10000
+fly_middle_reg = 0.01/flock_size
+speed_match_reg = 0.125/flock_size
+scaling = 2
+
+
+boids_x_coords=[random.uniform(init_x_coord[0], init_x_coord[1]) for x in range(flock_size)]
+boids_y_coords=[random.uniform(init_y_coord[0], init_y_coord[1]) for x in range(flock_size)]
+boid_x_velos=[random.uniform(init_x_velo[0], init_x_velo[1]) for x in range(flock_size)]
+boid_y_velos=[random.uniform(init_y_velo[0], init_y_velo[1]) for x in range(flock_size)]
+boids=(boids_x_coords,boids_y_coords,boid_x_velos,boid_y_velos)
 
 def update_boids(boids):
     x_coord,y_coord,x_velo,y_velo=boids
     # Fly towards the middle
-    for i in range(50):
-        for j in range(50):
-            x_velo[i]=x_velo[i]+(x_coord[j]-x_coord[i])*0.01/len(x_coord)
-    for i in range(50):
-        for j in range(50):
-            y_velo[i]=y_velo[i]+(y_coord[j]-y_coord[i])*0.01/len(x_coord)
+    for i in range(flock_size):
+        for j in range(flock_size):
+            x_velo[i]=x_velo[i]+(x_coord[j]-x_coord[i])*fly_middle_reg
+    for i in range(flock_size):
+        for j in range(flock_size):
+            y_velo[i]=y_velo[i]+(y_coord[j]-y_coord[i])*fly_middle_reg
     # Fly away from nearby boids
-    for i in range(50):
-        for j in range(50):
-            if (x_coord[j]-x_coord[i])**2 + (y_coord[j]-y_coord[i])**2 < 100:
+    for i in range(flock_size):
+        for j in range(flock_size):
+            if (x_coord[j]-x_coord[i])**scaling + (y_coord[j]-y_coord[i])**scaling < avoid_distance:
                 x_velo[i]=x_velo[i]+(x_coord[i]-x_coord[j])
                 y_velo[i]=y_velo[i]+(y_coord[i]-y_coord[j])
     # Try to match speed with nearby boids
-    for i in range(50):
-        for j in range(50):
-            if (x_coord[j]-x_coord[i])**2 + (y_coord[j]-y_coord[i])**2 < 10000:
-                x_velo[i]=x_velo[i]+(x_velo[j]-x_velo[i])*0.125/len(x_coord)
-                y_velo[i]=y_velo[i]+(y_velo[j]-y_velo[i])*0.125/len(x_coord)
+    for i in range(flock_size):
+        for j in range(flock_size):
+            if (x_coord[j]-x_coord[i])**scaling + (y_coord[j]-y_coord[i])**scaling < flying_distance:
+                x_velo[i]=x_velo[i]+(x_velo[j]-x_velo[i])*speed_match_reg
+                y_velo[i]=y_velo[i]+(y_velo[j]-y_velo[i])*speed_match_reg
     # Move according to velocities
-    for i in range(50):
+    for i in range(flock_size):
         x_coord[i]=x_coord[i]+x_velo[i]
         y_coord[i]=y_coord[i]+y_velo[i]
 
