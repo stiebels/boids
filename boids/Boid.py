7d4a75d0f369789b64bb3bbf5c69d8b9bb92b57f
stiebels <simon.stiebellehner@gmail.com>
2016-12-29 14:50:48 +0100

    changed to augmented assignment statements

--- boids/Boid.py
+++ boids/Boid.py
@@ -11,27 +11,27 @@ class Boid(object):
     def fly_middle(self, Flock, fly_middle_strength=0.01):
         fly_middle_reg = fly_middle_strength / Flock.size
         for boid in Flock.boids:
-            self.x_velo = self.x_velo + (boid.x_coord - self.x_coord) * fly_middle_reg
-            self.y_velo = self.y_velo + (boid.y_coord - self.y_coord) * fly_middle_reg
+            self.x_velo += (boid.x_coord - self.x_coord) * fly_middle_reg
+            self.y_velo += (boid.y_coord - self.y_coord) * fly_middle_reg
 
 
-    def fly_away(self, Flock, distance_limit=100, scaling=2):
+    def fly_away(self, Flock, avoid_distance=100, scaling=2):
         for boid in Flock.boids:
             distance = (boid.x_coord - self.x_coord) ** scaling + (boid.y_coord - self.y_coord) ** scaling
-            if distance < distance_limit:
-                self.x_velo = self.x_velo + (self.x_coord - boid.x_coord)
-                self.y_velo = self.y_velo + (self.y_coord - boid.y_coord)
+            if distance < avoid_distance:
+                self.x_velo += self.x_coord - boid.x_coord
+                self.y_velo += self.y_coord - boid.y_coord
 
 
-    def match_speed(self, Flock, speed_match_strength=0.125, scaling=2, distance_limit=10000):
+    def match_speed(self, Flock, speed_match_strength=0.125, scaling=2, flying_distance=10000):
         speed_match_reg = speed_match_strength / Flock.size
         for boid in Flock.boids:
             distance = (boid.x_coord - self.x_coord) ** scaling + (boid.y_coord - self.y_coord) ** scaling
-            if distance < distance_limit:
-                self.x_velo = self.x_velo + (boid.x_velo - self.x_velo) * speed_match_reg
-                self.y_velo = self.y_velo + (boid.y_velo - self.y_velo) * speed_match_reg
+            if distance < flying_distance:
+                self.x_velo += (boid.x_velo - self.x_velo) * speed_match_reg
+                self.y_velo += (boid.y_velo - self.y_velo) * speed_match_reg
 
 
     def move(self):
-        self.x_coord = self.x_coord + self.x_velo
-        self.y_coord = self.y_coord + self.y_velo
+        self.x_coord += self.x_velo
+        self.y_coord += self.y_velo
