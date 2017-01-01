7ede65790c552844058c41c665313a266944092e
stiebels <simon.stiebellehner@gmail.com>
2017-01-01 17:20:19 +0100

    added test cases for class Animator

--- boids/tests/test_animator.py
+++ boids/tests/test_animator.py
@@ -1,18 +1,33 @@
-from matplotlib import pyplot as plt
-from matplotlib import animation
-from boids.Flock import Flock
-import yaml
+import pickle
 import os
-from mock import patch, MagicMock
-from pytest import raises
+import random
+from mock import patch
+from boids.Animator import Animator
+
+
+def load_ani_fflock_fixture():
+    # Loads a predefined format_flock result
+    directory = str(os.path.dirname(os.path.abspath('__file__')) + '/fixtures/')
+    file = open(directory+"animator_fflock.p",'rb')
+    m_animator_fflock = pickle.load(file)
+    file.close()
+    return m_animator_fflock
 
 
 def test_format_flock():
+    random.seed(0)
+    t_Animator = Animator(size=50, fly_middle_strength=0.01, fly_away_limit=100,
+             speed_match_strength=0.125, distance_limit=10000, path=str(os.path.dirname(os.path.abspath('__file__')) + '/test_file.mp4'),
+             frames=5, config=False)
+    assert(t_Animator.format_flock() == load_ani_fflock_fixture())
 
 
-
-
-def test_animate():
-
-
+def test_Animator_init():
+    with patch.object(Animator, '__init__', return_value=None) as m_Animator:
+        t_Animator = Animator(size=50, fly_middle_strength=0.01, fly_away_limit=100,
+                              speed_match_strength=0.125, distance_limit=10000, path='dummy',
+                              frames=5, config=False)
+        m_Animator.assert_called_with(size=50, fly_middle_strength=0.01, fly_away_limit=100,
+                                      speed_match_strength=0.125, distance_limit=10000, path='dummy',
+                                      frames=5, config=False)
 
